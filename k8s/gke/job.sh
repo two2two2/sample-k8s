75efc904023353cdc9323e2b44d9edba017929be
ryom0624 <ryo.m0624@gmail.com>
2019-06-13 13:34:51 +0900

    job debug

--- k8s/gke/job.sh
+++ k8s/gke/job.sh
@@ -4,7 +4,7 @@ set -ex
 
 echo "start migration"
 sleep 2s
-trap "touch /tmp/pod/terminated && echo 'terminated file was created' && ls /tmp/pod" EXIT
+trap "touch /tmp/pod/terminated && echo 'terminated file was created'" EXIT
 bundle exec rails db:create
 bundle exec rails db:migrate
 echo "finished migration"
